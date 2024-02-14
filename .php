bf360ff615b563efb12728c828e516afd042b6c2
HYp3r42 <9021951@student.tcrmbo.nl>
2024-02-14 10:36:47 +0100

    aanpassing

---
+++ .php
@@ -0,0 +1,8 @@
+<?php
+<a href="#"><img src="logo.png" alt="Logo"></a>
+
+<ul>
+    <li><a href="#">Inloggen</a></li>
+    <li><a href="#">Klantenservice</a></li>
+</ul>
+?>
---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<?php
+// Controleer of het formulier is verzonden
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Controleer gebruikersnaam en wachtwoord (hier gewoon voorbeeld, je moet dit vervangen door echte authenticatie)
+    $username = "gebruiker";
+    $password = "wachtwoord";
+
+    if ($_POST["username"] == $username && $_POST["password"] == $password) {
+        echo "<h2>Welkom, $username!</h2>";
+    } else {
+        echo "<h2>Ongeldige gebruikersnaam of wachtwoord</h2>";
+    }
+}
+?>
+
+<h2>Inloggen</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label for="username">Gebruikersnaam:</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Wachtwoord:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Inloggen">
+</form>
+
+<h2>Aanmelden</h2>
+<p>Nog geen account? <a href="register.php">Registreer hier</a>.</p>
+
+</body>
+</html>
