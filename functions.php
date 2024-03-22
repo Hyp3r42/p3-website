7c243d09ffe7e88034ec0785bc23045754420a1d
Rknub <emir5314@outlook.com>
2024-03-22 12:31:58 +0100

    .

--- functions.php
+++ functions.php
@@ -1,43 +1,36 @@
-<!-- functions.php -->
 <?php
 require_once 'config.php';
 
-function get_records() {
-    global $database_file;
-    if(file_exists($database_file)) {
-        $records = file($database_file, FILE_IGNORE_NEW_LINES);
-        return array_map('json_decode', $records);
-    } else {
-        return [];
+function get_klanten() {
+    global $conn;
+    $sql = "SELECT * FROM klant";
+    $result = $conn->query($sql);
+    $klanten = [];
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $klanten[] = $row;
+        }
     }
+    return $klanten;
 }
 
-function add_record($record) {
-    global $database_file;
-    file_put_contents($database_file, json_encode($record) . PHP_EOL, FILE_APPEND);
+function get_producten() {
+    global $conn;
+    $sql = "SELECT * FROM product";
+    $result = $conn->query($sql);
+    $producten = [];
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $producten[] = $row;
+        }
+    }
+    return $producten;
 }
 
-function update_record($index, $new_record) {
-    global $database_file;
-    $records = get_records();
-    if(isset($records[$index])) {
-        $records[$index] = $new_record;
-        file_put_contents($database_file, implode(PHP_EOL, array_map('json_encode', $records)));
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function delete_record($index) {
-    global $database_file;
-    $records = get_records();
-    if(isset($records[$index])) {
-        unset($records[$index]);
-        file_put_contents($database_file, implode(PHP_EOL, array_map('json_encode', $records)));
-        return true;
-    } else {
-        return false;
-    }
+function add_bestelling($klant_id, $product_id, $hoeveelheid) {
+    global $conn;
+    $sql = "INSERT INTO bestelling (klant_id, product_id, hoeveelheid) VALUES ('$klant_id', '$product_id', '$hoeveelheid')";
+    $result = $conn->query($sql);
+    return $result;
 }
 ?>
