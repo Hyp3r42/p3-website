dbca6aa90d4ebc9f452a8d9008fa84da72825907
HYp3r42 <9021951@student.tcrmbo.nl>
2024-04-02 09:03:20 +0200

    .

--- Nike.php
+++ Nike.php
@@ -12,7 +12,7 @@
     $products = [
         ["name" => "Nike Air Max 90 Scream Green", "price" => "€178", "sizes" => ["42", "43", "44", "45", "46"], "image" => "img nike/1 (11).webp"],
         ["name" => "Nike Dunk High Game Royal", "price" => "€161", "sizes" => ["42", "43", "44", "45", "46"], "image" => "img nike/1 (12).webp"],
-        ["name" => "Peak sneaker black", "price" => "€49,99", "sizes" => ["42", "43", "44", "45", "46"], "image" => "img/_f4fe6d71-954c-4a5b-b887-42768475cf83.jpeg"],
+        ["name" => "Peak sneaker black", "price" => "€49,99", "sizes" => ["42", "43", "44", "45", "46"], "image" => "img nike/1 (13).webp"],
         ["name" => "Peak sneaker", "price" => "€149,99", "sizes" => ["41", "42", "43", "44", "45", "46", "47"], "image" => "sneaker.jpg"],
         ["name" => "Peak adidas sneaker grey", "price" => "€149,99", "sizes" => ["41", "42", "43", "44", "45", "46", "47"], "image" => "grey.jpg"],
         ["name" => "Peak sneaker green", "price" => "€149,99", "sizes" => ["42", "43", "44", "45", "46"], "image" => "green.jpg"]
