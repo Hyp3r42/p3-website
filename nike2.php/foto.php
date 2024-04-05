294eda006a6df71d95c2e9339d2bb7ed96d038a9
HYp3r42 <9021951@student.tcrmbo.nl>
2024-04-05 10:15:53 +0200

    .

--- nike2.php/foto.php
+++ nike2.php/foto.php
@@ -1,3 +1,12 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PEAK</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<img src="nike2.php/1 (16).webp" alt="PEAK logo" width="618" height="650">
+</body>
+</html>
